9e9c063a8087d3ff392d0bfa3a22a8da41903cc0
Luis Fernando Neu <luis.neu@gmail.com>
2025-04-21 17:50:13 -0300

    juaster provider

--- providers.tf
+++ providers.tf
@@ -12,25 +12,24 @@ terraform {
   }
   required_version = "~> 1.5.7"
 }
-
 provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.eks.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority.0.data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.eks.name]
-      command     = "aws"
+    kubernetes {
+        host                   = data.aws_eks_cluster.poc-cluster.endpoint
+        cluster_ca_certificate = base64decode(data.aws_eks_cluster.poc-cluster.certificate_authority.0.data)
+            exec {
+                api_version = "client.authentication.k8s.io/v1beta1"
+                args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.poc-cluster.name]
+                command     = "aws"
+            }
     }
-  }
 }
 
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.poc-cluster.token
-  #load_config_file       = false
+    host                   = data.aws_eks_cluster.poc-cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.poc-cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.poc-cluster.token
+    #load_config_file       = false
 }
 
 provider "aws" {
