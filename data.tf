4138522e8c82bf7490ad5a066bece380f1d4dabc
Luis Fernando Neu <luisneu@gmail.com>
2025-05-16 10:08:15 -0300

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,7 +1,9 @@
 data "aws_eks_cluster" "poc-cluster" {
-  name = module.my-cluster.cluster_name
+  name = module.my-cluster.cluster_id # create cluster
+  #name = module.my-cluster.cluster_name # install helm
 }
 
 data "aws_eks_cluster_auth" "poc-cluster" {
-  name = module.my-cluster.cluster_name
+  name = module.my-cluster.cluster_id  # create cluster
+  #name = module.my-cluster.cluster_name # install helm
 }
